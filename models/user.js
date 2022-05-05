2fffb3f55fb0ee68036271b279d52ce481b3a2a1
LillianWight <lillian.wight@gmail.com>
2022-05-05 11:43:56 -0600

    End of Lesson 41 Virtual Fields and Methods

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uuidv1 = require("uuidv1");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,5 +23,30 @@ const userSchema = new mongoose.Schema({
   },
   updated: Date,
 });
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update("password")
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
